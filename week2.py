4a808efddd76fc51f0b8117dedc51ca1ff922dd0
RasmusHaarslev <rasmus.haarslev@gmail.com>
2015-09-15 12:12:40 +0200

    hej

--- week2.py
+++ week2.py
@@ -1,11 +1,13 @@
-import itertools, json
+import json
+import re
+
 
 def read_matrix(filename):
     f = open(filename, 'r')
     lines = f.readlines()
     matrix_list = []
     for line in lines:
-        matrix_list.append([c for c in line.rstrip().split(' ')])
+        matrix_list.append([c for c in re.split("[, ]", line.rstrip())])
     f.close()
     return matrix_list
 
@@ -22,7 +24,11 @@ def write_matrix(array, filename):
 
 
 def bit_strings(N):
-    return list(itertools.product(range(2), repeat=N))
+    pools = [[0, 1]] * N
+    result = [[]]
+    for pool in pools:
+        result = [x+[y] for x in result for y in pool]
+    return result
 
 
 def bag_of_words(filename):
@@ -39,7 +45,7 @@ def bag_of_words(filename):
 
 
 if "__main__" == __name__:
-    #print read_matrix('matrix')
+    print read_matrix('matrix')
     #write_matrix([[2, 3], [1, 4]], 'matrix2')
     #print bit_strings(3)
-    print bag_of_words("pizza-train.json")
+    #print bag_of_words("pizza-train.json")
