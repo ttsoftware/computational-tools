02d4f7d43f4341e147fcdef127966b59f0ed5f8d
Troels Thomsen <tt.ttsoftware@gmail.com>
2015-09-22 13:38:22 +0200

    3.3 done

--- week3.py
+++ week3.py
@@ -44,21 +44,69 @@ def panda_top_movies(movie_data):
     top_five = ratings_by_movie.sort(columns='rating count', ascending=False).iloc[0:5]
     active_titles = ratings_by_movie[ratings_by_movie['rating count'] >= 250]
 
-    #print top_five
-    #print active_titles
-
     movies_ratings = movie_data.merge(active_titles.reset_index(), on='movie id')
     # filter gender
     female_ratings = movies_ratings[movies_ratings['gender'].isin(['F'])]
     male_ratings = movies_ratings[movies_ratings['gender'].isin(['M'])]
 
-    print female_ratings.groupby(['movie id'])['rating'].agg(['mean']).sort(columns='mean', ascending=False).iloc[0:3]
+    top_3_female_ratings = (
+        female_ratings
+        .groupby(['movie id'])['rating']
+        .agg(['mean'])
+        .sort(columns='mean', ascending=False)
+        .iloc[0:3]
+    )
+    top_3_male_ratings = (
+        male_ratings
+        .groupby(['movie id'])['rating']
+        .agg(['mean'])
+        .sort(columns='mean', ascending=False)
+        .iloc[0:3]
+    )
 
+    gender_means = (
+        female_ratings
+        .groupby(['movie id'])['rating']
+        .agg(['mean'])
+        .reset_index()
+        .merge(
+            (
+                male_ratings
+                .groupby(['movie id'])['rating']
+                .agg(['mean'])
+                .reset_index()
+            ),
+            on='movie id'
+        )
+    )
+
+    gender_means['mean_diff'] = gender_means['mean_x'] - gender_means['mean_y']
+    gender_means_diff_sorted = gender_means.sort(columns='mean_diff', ascending=False)
+
+    top_10_female_mean_diff = gender_means_diff_sorted.iloc[0:10]
+    top_10_male_mean_diff = gender_means_diff_sorted.iloc[-10:].sort(columns='mean_diff', ascending=True)
+
+    top_5_std_rating = (
+        movies_ratings
+        .groupby(['movie id'])['rating']
+        .agg(['std'])
+        .sort(columns='std', ascending=False)
+        .iloc[0:5]
+    )
+
+    return (
+        top_five,
+        active_titles,
+        top_3_female_ratings,
+        top_3_male_ratings,
+        top_10_female_mean_diff,
+        top_10_male_mean_diff,
+        top_5_std_rating
+    )
 
 
 if "__main__" == __name__:
     #print solve_matrix("matrix3")
     #print roots("ENyYffaq.txt")
-    # print roots('ENyYffaq.txt')
     movie_data = panda_movie_merge()
     panda_top_movies(movie_data)
